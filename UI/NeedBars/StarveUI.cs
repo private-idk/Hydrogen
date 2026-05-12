bb42f99a068ecb4551c6f971aea5fce7022bb548
private-idk <alexandr.penguin@gmail.com>
2026-05-12 17:09:19 +0300

    Commented bars

--- UI/NeedBars/StarveUI.cs
+++ UI/NeedBars/StarveUI.cs
@@ -8,59 +8,59 @@ namespace Hydrogen.UI.NeedBars;
 
 public class StarveUI : ModSystem
 {
-    private static Texture2D starveTex;
+    // private static Texture2D starveTex;
 
-    public static float generalXOffset;
-    public static float generalYOffset;
+    // public static float generalXOffset;
+    // public static float generalYOffset;
 
-    public override void OnModLoad()
-    {
-        starveTex = ModContent.Request<Texture2D>($"Terraria/Images/Item_{ItemID.Apple}").Value;
+    // public override void OnModLoad()
+    // {
+    //     starveTex = ModContent.Request<Texture2D>($"Terraria/Images/Item_{ItemID.Apple}").Value;
 
-        generalXOffset = Main.screenWidth / 4f;
-        generalYOffset = Main.inventoryScale * 52f;
-    }
+    //     generalXOffset = Main.screenWidth / 4f;
+    //     generalYOffset = Main.inventoryScale * 52f;
+    // }
 
-    public override void OnModUnload()
-    {
-        starveTex = null;
-    }
+    // public override void OnModUnload()
+    // {
+    //     starveTex = null;
+    // }
 
-    public static void Draw(SpriteBatch spriteBatch)
-    {
-        var bar = Utils.CenteredRectangle(new Vector2(generalXOffset + starveTex.Width * 3.5f, Main.inventoryScale * 78f), new Vector2(starveTex.Width * 5f, starveTex.Height / 2));
+    // public static void Draw(SpriteBatch spriteBatch)
+    // {
+    //     var bar = Utils.CenteredRectangle(new Vector2(generalXOffset + starveTex.Width * 3.5f, Main.inventoryScale * 78f), new Vector2(starveTex.Width * 5f, starveTex.Height / 2));
 
-        for (int i = 1; i < HydrogenPlayer.maxStarve / 20 + 1; i++)
-        {
-            Color color = new Color(255, 255, 255);
+    //     for (int i = 1; i < HydrogenPlayer.maxStarve / 20 + 1; i++)
+    //     {
+    //         Color color = new Color(255, 255, 255);
 
-            if (i * 20 > HydrogenPlayer.Starve)
-            {
-                if (i * 20 - 20 > HydrogenPlayer.Starve)
-                {
-                    color.R = 50;
-                    color.B = 50;
-                    color.G = 50;
-                }
-                else
-                {
-                    color.R = (byte)(255 - (i * 20 - HydrogenPlayer.Starve) * 10);
-                    color.B = (byte)(255 - (i * 20 - HydrogenPlayer.Starve) * 10);
-                    color.G = (byte)(255 - (i * 20 - HydrogenPlayer.Starve) * 10);
-                }
-            }
+    //         if (i * 20 > HydrogenPlayer.Starve)
+    //         {
+    //             if (i * 20 - 20 > HydrogenPlayer.Starve)
+    //             {
+    //                 color.R = 50;
+    //                 color.B = 50;
+    //                 color.G = 50;
+    //             }
+    //             else
+    //             {
+    //                 color.R = (byte)(255 - (i * 20 - HydrogenPlayer.Starve) * 10);
+    //                 color.B = (byte)(255 - (i * 20 - HydrogenPlayer.Starve) * 10);
+    //                 color.G = (byte)(255 - (i * 20 - HydrogenPlayer.Starve) * 10);
+    //             }
+    //         }
 
-            spriteBatch.Draw(starveTex,
-                new Vector2(generalXOffset + starveTex.Width * i, Main.inventoryScale * 52f),
-                new Rectangle(0, 0, starveTex.Width, starveTex.Height / 3),
-                color);
+    //         spriteBatch.Draw(starveTex,
+    //             new Vector2(generalXOffset + starveTex.Width * i, Main.inventoryScale * 52f),
+    //             new Rectangle(0, 0, starveTex.Width, starveTex.Height / 3),
+    //             color);
             
-        }
+    //     }
 
-        Rectangle mouseHitbox = new Rectangle((int)Main.MouseScreen.X, (int)Main.MouseScreen.Y, 8, 8);
-        if (mouseHitbox.Intersects(bar))
-        {
-            Main.instance.MouseText($"{HydrogenPlayer.Starve}/{HydrogenPlayer.maxStarve}");
-        }
-    }
+    //     Rectangle mouseHitbox = new Rectangle((int)Main.MouseScreen.X, (int)Main.MouseScreen.Y, 8, 8);
+    //     if (mouseHitbox.Intersects(bar))
+    //     {
+    //         Main.instance.MouseText($"{HydrogenPlayer.Starve}/{HydrogenPlayer.maxStarve}");
+    //     }
+    // }
 }
--- UI/NeedBars/ThirstUI.cs
+++ UI/NeedBars/ThirstUI.cs
@@ -8,53 +8,53 @@ namespace Hydrogen.UI.NeedBars;
 
 public class ThirstUI : ModSystem
 {
-    private static Texture2D thirstTex;
+    // private static Texture2D thirstTex;
 
-    public override void OnModLoad()
-    {
-        thirstTex = ModContent.Request<Texture2D>($"Terraria/Images/Item_{ItemID.WaterBucket}").Value;
-    }
+    // public override void OnModLoad()
+    // {
+    //     thirstTex = ModContent.Request<Texture2D>($"Terraria/Images/Item_{ItemID.WaterBucket}").Value;
+    // }
 
-    public override void OnModUnload()
-    {
-        thirstTex = null;
-    }
+    // public override void OnModUnload()
+    // {
+    //     thirstTex = null;
+    // }
 
-    public static void Draw(SpriteBatch spriteBatch)
-    {
-        var bar = Utils.CenteredRectangle(new Vector2(StarveUI.generalXOffset + thirstTex.Width * 3.5f, Main.inventoryScale * 78f), new Vector2(thirstTex.Width * 5f, thirstTex.Height / 2));
+    // public static void Draw(SpriteBatch spriteBatch)
+    // {
+    //     var bar = Utils.CenteredRectangle(new Vector2(StarveUI.generalXOffset + thirstTex.Width * 3.5f, Main.inventoryScale * 78f), new Vector2(thirstTex.Width * 5f, thirstTex.Height / 2));
 
-        for (int i = 1; i < HydrogenPlayer.maxThirst / 20 + 1; i++)
-        {
-            Color color = new Color(255, 255, 255);
+    //     for (int i = 1; i < HydrogenPlayer.maxThirst / 20 + 1; i++)
+    //     {
+    //         Color color = new Color(255, 255, 255);
 
-            if (i * 20 > HydrogenPlayer.Thirst)
-            {
-                if (i * 20 - 20 > HydrogenPlayer.Thirst)
-                {
-                    color.R = 50;
-                    color.B = 50;
-                    color.G = 50;
-                }
-                else
-                {
-                    color.R = (byte)(255 - (i * 20 - HydrogenPlayer.Thirst) * 10);
-                    color.B = (byte)(255 - (i * 20 - HydrogenPlayer.Thirst) * 10);
-                    color.G = (byte)(255 - (i * 20 - HydrogenPlayer.Thirst) * 10);
-                }
-            }
+    //         if (i * 20 > HydrogenPlayer.Thirst)
+    //         {
+    //             if (i * 20 - 20 > HydrogenPlayer.Thirst)
+    //             {
+    //                 color.R = 50;
+    //                 color.B = 50;
+    //                 color.G = 50;
+    //             }
+    //             else
+    //             {
+    //                 color.R = (byte)(255 - (i * 20 - HydrogenPlayer.Thirst) * 10);
+    //                 color.B = (byte)(255 - (i * 20 - HydrogenPlayer.Thirst) * 10);
+    //                 color.G = (byte)(255 - (i * 20 - HydrogenPlayer.Thirst) * 10);
+    //             }
+    //         }
 
-            spriteBatch.Draw(thirstTex,
-                new Vector2(StarveUI.generalXOffset + thirstTex.Width * i, Main.inventoryScale * 26f),
-                // new Rectangle(0, 0, thirstTex.Width, thirstTex.Height / 3),
-                color);
+    //         spriteBatch.Draw(thirstTex,
+    //             new Vector2(StarveUI.generalXOffset + thirstTex.Width * i, Main.inventoryScale * 26f),
+    //             // new Rectangle(0, 0, thirstTex.Width, thirstTex.Height / 3),
+    //             color);
             
-        }
+    //     }
 
-        Rectangle mouseHitbox = new Rectangle((int)Main.MouseScreen.X, (int)Main.MouseScreen.Y, 8, 8);
-        if (mouseHitbox.Intersects(bar))
-        {
-            Main.instance.MouseText($"{HydrogenPlayer.Thirst}/{HydrogenPlayer.maxThirst}");
-        }
-    }
+    //     Rectangle mouseHitbox = new Rectangle((int)Main.MouseScreen.X, (int)Main.MouseScreen.Y, 8, 8);
+    //     if (mouseHitbox.Intersects(bar))
+    //     {
+    //         Main.instance.MouseText($"{HydrogenPlayer.Thirst}/{HydrogenPlayer.maxThirst}");
+    //     }
+    // }
 }
