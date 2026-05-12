20afbad9f2f5a2e77b46df084d4c2392f6540315
EL-Linguist <privka83@gmail.com>
2026-05-12 20:03:22 +0500

    Nothing

---
+++ Common/AllNPC/SlimeNPC.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Hydrogen.Common.AllNPC
+{
+    public class SlimeNPC : GlobalNPC
+    {
+        public override bool AppliesToEntity(NPC entity, bool lateInstantiation)
+        {
+            if (entity.type == NPCID.GreenSlime)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Content/Buffs/PlayerSlimed.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hydrogen.Content.Buffs
+{
+    internal class PlayerSlimed
+    {
+    }
+}
