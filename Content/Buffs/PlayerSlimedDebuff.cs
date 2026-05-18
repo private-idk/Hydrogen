7be863618a6243b4d312136e33caf76a11ea8bb9
private-idk <alexandr.penguin@gmail.com>
2026-05-18 17:00:05 +0300

    Completed PlayerSlimedDebuff

--- Content/Buffs/PlayerSlimedDebuff.cs
+++ Content/Buffs/PlayerSlimedDebuff.cs
@@ -6,12 +6,13 @@ namespace Hydrogen.Content.Buffs;
 public class PlayerSlimedDebuff : ModBuff
 {
     private int[] _fireItems = [
-        ItemID.Hellstone, ItemID.HellstoneBar, ItemID.HellstoneBrick, ItemID.HellstoneBrickWall, ItemID.LivingFireBlock, ItemID.Torch, ItemID.Candle, ItemID.Campfire, ItemID.Fireplace,
-        ItemID.HellfireTreads, ItemID.FireWhip, ItemID.HelFire, ItemID.VolcanoLarge, ItemID.VolcanoSmall,
-        ItemID.MoltenFury, ItemID.Flamarang,ItemID.Sunfury, ItemID.PhoenixBlaster, ItemID.Flamelash,
-        ItemID.MoltenHamaxe, ItemID.MoltenCharm, ItemID.MoltenSkullRose, ItemID.MoltenQuiver, ItemID.MoltenPickaxe,
-        ItemID.SolarFlareAxe, ItemID.SolarFlareChainsaw, ItemID.SolarFlareDrill, ItemID.SolarFlareHammer, ItemID.FlowerofFire,
-        ItemID.FireGauntlet, ItemID.FireFeather, 
+        ItemID.Hellstone, ItemID.HellstoneBar, ItemID.HellstoneBrick, ItemID.HellstoneBrickWall, ItemID.LivingFireBlock,
+        ItemID.Torch, ItemID.Candle, ItemID.Campfire, ItemID.Fireplace, ItemID.HellfireTreads,
+        ItemID.FireWhip, ItemID.HelFire, ItemID.VolcanoLarge, ItemID.VolcanoSmall, ItemID.MoltenFury,
+        ItemID.Flamarang,ItemID.Sunfury, ItemID.PhoenixBlaster, ItemID.Flamelash, ItemID.MoltenHamaxe,
+        ItemID.MoltenCharm, ItemID.MoltenSkullRose, ItemID.MoltenQuiver, ItemID.MoltenPickaxe, ItemID.SolarFlareAxe,
+        ItemID.SolarFlareChainsaw, ItemID.SolarFlareDrill, ItemID.SolarFlareHammer, ItemID.FlowerofFire, ItemID.FireGauntlet,
+        ItemID.FireFeather, 
     ];
 
     // Если поблизости игрока будут эти тайлы, то он загорится
@@ -39,6 +40,35 @@ public class PlayerSlimedDebuff : ModBuff
                 {
                     player.AddBuff(BuffID.Burning, 2);
                     break;
+                }
+            }
+        }
+
+        int tileX = (int)(player.position.X / 16f);
+        int tileY = (int)(player.position.Y / 16f);
+
+        for (int i = -3; i <= 3; i++)
+        {
+            for (int j = -3; j <= 3; j++)
+            {
+                int x = tileX + i;
+                int y = tileY + j;
+
+                if (!WorldGen.InWorld(x, y))
+                    continue;
+
+                var tile = Framing.GetTileSafely(x, y);
+
+                if (tile.HasTile)
+                {
+                    foreach (var t in _tiles)
+                    {
+                        if (t == tile.TileType)
+                        {
+                            player.AddBuff(BuffID.Burning, 2);
+                            break;
+                        }
+                    }
                 }
             }
         }
